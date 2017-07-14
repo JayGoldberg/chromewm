4fd4f4337ba4e098ea7cbb47991965df6975bbaf
EduCampi <edu@campi.xyz>
2017-07-14 01:22:00 -0300

    Switched to .jar compiler

--- build.sh
+++ build.sh
@@ -6,11 +6,18 @@
 ############################################################
 # Replace the following with the paths in your environment #
 ############################################################
-JS_CLOSURE_COMPILER_PATH=../third_party/bin/closure-compiler.jar
-CLOSURE_BUILDER_PATH=../third_party/closure-library/closure/bin/build/closurebuilder.py
-CLOSURE_LIBRARY_PATH=../third_party/closure-library/
-JS_CHROME_EXTENSIONS_API=../third_party/closure-compiler/contrib/externs/chrome_extensions.js
-COMPILED_PATH=bin
+OUTPUT_PATH="bin"
+JS_CLOSURE_COMPILER_PATH="../third_party/bin/closure-compiler.jar"
+JS_CLOSURE_LIBRARY_PATH="../third_party/closure-library"
+JS_COMPILER_LEVEL="SIMPLE"
+  # SIMPLE: Makes debugging Errors easier as variables keep their names
+  # ADVANCED: Switch to ADVANCED during final builds
+
+# Add all necessary externs
+JS_EXTERNS="\
+../third_party/closure-compiler/contrib/externs/chrome_extensions.js \
+"
+
 
 ###################################
 #  Do not modify bellow this line #
@@ -19,8 +26,19 @@ NC='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 
-JS_COMPILER_PARAMETERS="\
---compilation_level=SIMPLE \
+JS_COMPILER_BASIC="\
+--compilation_level=$JS_COMPILER_LEVEL \
+--dependency_mode=STRICT \
+"
+
+JS_COMPILER_EXTRAS="\
+--externs=$JS_EXTERNS \
+--js \"$JS_CLOSURE_LIBRARY_PATH/closure/goog/**.js\" \
+--js \"\!$JS_CLOSURE_LIBRARY_PATH/closure/goog/**test.js\" \
+--js \"./**.js\" \
+"
+
+JS_COMPILER_CHECKS="\
 --jscomp_error=missingRequire \
 --jscomp_warning=accessControls \
 --jscomp_warning=constantProperty \
@@ -40,20 +58,33 @@ JS_COMPILER_PARAMETERS="\
 --jscomp_warning=underscore \
 --jscomp_off=reportUnknownTypes \
 --jscomp_off=missingProperties \
---externs=$JS_CHROME_EXTENSIONS_API \
---hide_warnings_for=goog"
-
+--hide_warnings_for=goog "
 #--jscomp_off=unknownDefines \
 #--jscomp_off=undefinedVars \
 #--jscomp_off=undefinedNames \
-# TODO: Replace closure builder by compiler using dependency_mode
-JS_BUILDER_PARAMETERS="\
---compiler_jar=$JS_CLOSURE_COMPILER_PATH \
---root=$CLOSURE_LIBRARY_PATH \
---root=js/ \
---output_mode=compiled \
---compiler_flags=\"$JS_COMPILER_PARAMETERS\""
+#TODO(): Unhide goog and enable and solve the warnings flagged as off?
 
+
+JS_COMPILER_PARAMETERS="\
+$JS_COMPILER_BASIC $JS_COMPILER_CHECKS $JS_COMPILER_EXTRAS\
+"
+
+JS_COMPILER="java -jar $JS_CLOSURE_COMPILER_PATH"
+
+###### Functions ######
+function getNamespace {
+  while read line; do
+    if [[ "$line" == "goog.provide"* ]]; then
+      lineMinusPrefix=${line#*\'}
+      nameSpace=${lineMinusPrefix%\'*}
+      echo $nameSpace
+      return
+    fi
+  done < $1
+}
+
+###### Actual Program Starts bellow ######
+mkdir -p $OUTPUT_PATH
 
 while read TARGET; do
   [[ "$TARGET" == "#"* || -z "$TARGET" ]] && continue
@@ -62,19 +93,22 @@ while read TARGET; do
   FILENAME=`basename $TARGET`
   # Compiles only if the source file changed.
   # TODO(): Compile if dependencies changed
- [ ! $TARGET -nt $COMPILED_PATH/$FILENAME ] && continue
+ [ ! $TARGET -nt $OUTPUT_PATH/$FILENAME ] && continue
 
   case $TARGET in
     # Compile if it's a JavaScript file
     *.js)
       echo -e "${GREEN}Compiling${NC} $TARGET"
-      BUILD_TARGET=" --input=$TARGET --output_file=$COMPILED_PATH/$FILENAME"
-      BUILD_COMMAND="$CLOSURE_BUILDER_PATH $JS_BUILDER_PARAMETERS $BUILD_TARGET"
+      NAME_SPACE=$(getNamespace $TARGET)
+      BUILD_TARGET="--entry_point=$NAME_SPACE --js_output_file=$OUTPUT_PATH/$FILENAME"
+      BUILD_COMMAND="$JS_COMPILER $JS_COMPILER_PARAMETERS $BUILD_TARGET"
       eval $BUILD_COMMAND
       ;;
     # Copy everything else
+    # TODO(): Allow to use wildcards in BUILD_TARGETS file
     ?*)
       echo -e "${GREEN}Coping${NC} $TARGET"
-      cp $TARGET $COMPILED_PATH/$FILENAME
+      cp $TARGET $OUTPUT_PATH/$FILENAME
   esac
 done < BUILD_TARGETS
+echo -e "${GREEN}Build Completed!${NC}"
--- makefile
+++
@@ -1,7 +0,0 @@
-CLOSURE_LIBRARY_PATH="../thirdparty/closure-library/"
-JS_COMPILER_PATH="../thirdparty/bin/closure-compiler.jar"
-EXTERNS="../thirdparty/closure-compiler/contrib/externs/chrome_extensions.js"
-
-
-background: js/background.js
-	echo $(EXTERNS)
